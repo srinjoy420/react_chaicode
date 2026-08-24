d9ebd0802f086c32e033cd5bbacd45ef19e163dc
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-08-24 19:27:50 +0530

    added

--- day1/src/App.jsx
+++ day1/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Chai from './Chai'
 
 
 const App = () => {
   const username="hello"
+  let[counter,setcounter]=useState(15)
+  const addvalue=()=>{
+    setcounter(counter+1)
+  }
   return (
-    <div><Chai/> {username}</div>
+    <div>
+      <h2>counter value:{counter}</h2>
+      <button onClick={addvalue}>count++</button>
+      <button>count--</button>
+    </div>
   )
 }
 
