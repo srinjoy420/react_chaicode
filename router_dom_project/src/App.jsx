96ed3d691dece8f81d3bf76fb50f75741e73bb0c
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-09-12 19:16:55 +0530

    some changes

--- router_dom_project/src/App.jsx
+++ router_dom_project/src/App.jsx
@@ -1,39 +1,40 @@
+import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/footer/Footer'
-import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider } from 'react-router'
+import { Outlet,Routes,Router,Route } from 'react-router'
 import Home from './components/Home/Home'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
-import Github from './components/github/Github'
-import { giHubLoader } from './components/github/githubLoader'
+import Github, { giHubLoader } from './components/github/Github'
 import User from './components/user/User'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route loader={giHubLoader} path='github' element={<Github />} />
-      <Route path='user/:userId' element={<User />} />
-    </Route>,
-  ),
-)
-
-function Layout() {
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen">
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  )
-}
 
 const App = () => {
-  return <RouterProvider router={router} />
+  function Layout(){
+    return(
+     <>
+        <Header/>
+      <main className="min-h-screen">
+        <Outlet/>
+      </main>
+      <Footer/>
+     </>
+    )
+  }
+  return (
+    <div>
+     <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/github' element={<Github/>}/>
+        <Route path='/user/:userId' element={<User/>}/>
+       
+      </Route>
+     </Routes>
+    </div>
+  )
 }
 
 export default App
--- router_dom_project/src/components/github/Github.jsx
+++ router_dom_project/src/components/github/Github.jsx
@@ -1,14 +1,25 @@
-import { useLoaderData } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import {useLoaderData} from "react-router"
 
 const Github = () => {
-    const data = useLoaderData()
+    // const data=useLoaderData()
+    const [data, useData] = useState([])
+    useEffect(() => {
+        fetch('https://api.github.com/users/srinjoy420')
+            .then((res) => res.json())
+            .then((data) => useData(data))
 
+    }, [])
     return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
-            Github followers: {data.followers}
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
             <img src={data.avatar_url} alt="Git picture" width={300} />
         </div>
     )
 }
 
 export default Github
+
+export const giHubLoader=async()=>{
+    const res=await fetch(`https://api.github.com/users/srinjoy420`)
+    return res.json()
+}
